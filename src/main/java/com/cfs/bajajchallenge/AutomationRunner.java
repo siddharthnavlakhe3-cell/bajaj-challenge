6482a91e07ec399be632ba480f4647209c92afed
Siddharth Navlakhe <siddharthnavlakhe3@gmail.com>
2026-05-25 20:41:27 +0530

    Initial Setup

---
+++ src/main/java/com/cfs/bajajchallenge/AutomationRunner.java
@@ -0,0 +1,21 @@
+package com.cfs.bajajchallenge;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.*;
+import java.util.*;
+
+@Component
+public class AutomationRunner implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Application Started for Siddharth Navlakhe...");
+
+        // PRACTICE AREA: Kal yahan logic paste karenge
+        // Name: Siddharth Navlakhe
+        // Enrollment: 0827CS231254
+        // Email: siddharthnavlakhe230824@acropolis.in
+    }
+}
---
+++ src/main/java/com/cfs/bajajchallenge/BajajChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.cfs.bajajchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BajajChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BajajChallengeApplication.class, args);
+    }
+
+}
